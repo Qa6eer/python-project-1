6139adc62cef3d416e5f1fc61eac1b74ad63731e
ABDUL QADEER KHANDAY <qa6eer@gmail.com>
2024-12-27 08:48:03 +0530

    Update 10.py

--- python project/python project/10.py
+++ python project/python project/10.py
@@ -3,5 +3,7 @@ char = input("Enter an alphabet: ")
 if char.isalpha():
     if char.isupper():
         print(char, "is an uppercase alphabet.")
-    else:  
+    else:
         print(char, "is a lowercase alphabet.")
+else:
+    print("Invalid input! Please enter an alphabet.")
